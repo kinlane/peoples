2c58988958b15628897fe713b147bee0ee6eb1f3
Kin Lane <kinlane@gmail.com>
2022-12-12 15:34:04 -0800

    update bookmarklet

--- peoples/bookmarklet/get/index.js
+++ peoples/bookmarklet/get/index.js
@@ -13,18 +13,44 @@ exports.handler = vandium.generic()
 
     if(event.name && event.name != '' && event.key == 'akkess'){
         
-        var sql = 'INSERT INTO peoples(name,description,url)';
-
-        sql += " VALUES('" + event.name + "','" + event.description + "','" + event.url + "')";
-  
-        connection.query(sql, function (error, results, fields) {
+        if(event.type == 'twitter'){
+            var psql = "INSERT INTO peoples(name,description,twitt_url) VALUES('" + event.name + "','" + event.description + "','" + event.url + "')";
+        }
+        else if(event.type == 'linkedin'){
+            var psql = "INSERT INTO peoples(name,description,linkedkin_url) VALUES('" + event.name + "','" + event.description + "','" + event.url + "')";
+        }
+        else if(event.type == 'blog'){
+            var psql = "INSERT INTO peoples(name,description,blog_url) VALUES('" + event.name + "','" + event.description + "','" + event.url + "')";
+        }     
+        else if(event.type == 'mastodon'){
+            var psql = "INSERT INTO peoples(name,description,mastodon_url) VALUES('" + event.name + "','" + event.description + "','" + event.url + "')";
+        }  
+        else if(event.type == 'github'){
+            var psql = "INSERT INTO peoples(name,description,github_url) VALUES('" + event.name + "','" + event.description + "','" + event.url + "')";
+        }          
+        else if(event.type == 'url'){
+            var psql = "INSERT INTO peoples(name,description,blog_url) VALUES('" + event.name + "','" + event.description + "','" + event.url + "')";    
+        }
+        connection.query(psql, function (error, presults, fields) {
       
-          var response = {};
-          response['id'] = results.insertId;
-          response['name'] = event.name;
-    
-          callback( null, response );
-    
+            // Check Tag
+            var tsql = "SELECT * FROM tags WHERE name = '" + event.tag + "'";
+            connection.query(tsql, function (error, tresults, fields) {  
+                
+                var tag_id = tresults[0].id;
+                
+                // Insert Tag
+                var ptsql = "INSERT INTO peoples_tags(people_id,tag_id) VALUES(" + results.insertId + "," + tag_id + ")";
+                connection.query(ptsql, function (error, ptresults, fields) {    
+                    
+                  var response = {};
+                  response['id'] = presults.insertId;
+                  response['name'] = event.name;                
+                    
+                    callback( null, response );
+                });
+            
+            });
         });
     }
     else{
